8dd7f110a9c6760588b4d51f1395330b33d07c60
Adrian Stachura <48847246+AdrianHStachura@users.noreply.github.com>
2025-05-30 10:09:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 
 let num_of_cookies = 0;
+let multipler = 1;
 document.getElementById("num_of_cookies").textContent = num_of_cookies;
 
 
 
 function handleClick(){
-    num_of_cookies ++;
+    num_of_cookies = num_of_cookies + (1 * multipler);
     document.getElementById("num_of_cookies").textContent = num_of_cookies;
     const image = document.querySelector(".cookie img");
     image.classList.remove('animate'); void image.offsetWidth; image.classList.add('animate');
